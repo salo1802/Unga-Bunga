7f3dbdf544410f39568a212e3a29e0ae10ac9e3f
CamiloGuevara19 <camilogr0419@gmail.com>
2021-06-08 16:01:18 -0500

    Added basic classes

---
+++ src/view/Main.java
@@ -0,0 +1,48 @@
+package view;
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("view.Main");
+	}
+	
+	@Override
+	public void settings() {
+		size(500,500);
+	}
+	
+	@Override
+	public void setup() {
+		
+	}
+	
+	@Override
+	public void draw() {
+		background(255);	
+	}
+
+	
+	@Override
+	public void keyPressed() {
+		
+	}
+	
+	@Override
+	public void mousePressed() {
+		
+	}
+	
+	@Override
+	public void mouseDragged() {
+		
+	}
+	
+	@Override
+	public void mouseReleased() {
+		
+	}
+
+	}
+
+
